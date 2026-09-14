0644bcaa7c22b46123112a9e1391c2c05b508ca0
rahulaie882 <rahulr79469@gmail.com>
2026-09-14 23:30:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,8 +15,8 @@ from telegram.ext import ApplicationBuilder, CallbackQueryHandler, CommandHandle
 # ==========================================================
 # BASIC SETTINGS
 # ==========================================================
-BOT_TOKEN = "86894659686SUjzx5LJRcTPFE86OYnrJSEbaNlvag"
-ADMIN_ID = 8289958743
+BOT_TOKEN = "8931821917:AAFOm4YB7J4yi_vyQ-fZ7jwFMKWir8de3VU"
+ADMIN_ID = 8755368088
 
 # Start page photo aur QR. Baad me apne links/file_id laga sakte ho.
 MAIN_PHOTO = "https://nei1-ba101f5dd8aa.herokuapp.com/stream/177757?hash=61f1c7&d=true"
